10a20bb991f95a94be7c0b3899ec0eec821a3ed1
Denis Flaven <denis.flaven@combodo.com>
2020-07-08 17:08:39 +0200

    Cleanup of the default configuration

--- module.combodo-saml.php
+++ module.combodo-saml.php
@@ -57,59 +57,6 @@ SetupWebPage::AddModule(
 			'strict' => true,
 			// Enable debug mode (to print errors).
 			'debug' => false,
-			// Service Provider Data that we are deploying.
-			'sp' => array (
-				// Specifies info about where and how the <AuthnResponse> message MUST be
-				// returned to the requester, in this case our SP.
-				'assertionConsumerService' => array (
-					// SAML protocol binding to be used when returning the <Response>
-					// message. OneLogin Toolkit supports this endpoint for the
-					// HTTP-POST binding only.
-					'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-				),
-				// If you need to specify requested attributes, set a
-				// attributeConsumingService. nameFormat, attributeValue and
-				// friendlyName can be omitted
-				"attributeConsumingService"=> array(
-					"serviceName" => "",
-					"serviceDescription" => "",
-					"requestedAttributes" => array(
-						array(
-							"name" => "",
-							"isRequired" => false,
-							"nameFormat" => "",
-							"friendlyName" => "",
-							"attributeValue" => array()
-						)
-					)
-				),
-				// Specifies info about where and how the <Logout Response> message MUST be
-				// returned to the requester, in this case our SP.
-				'singleLogoutService' => array (
-					// SAML protocol binding to be used when returning the <Response>
-					// message. OneLogin Toolkit supports the HTTP-Redirect binding
-					// only for this endpoint.
-					'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-				),
-				// Specifies the constraints on the name identifier to be used to
-				// represent the requested subject.
-				// Take a look on lib/Saml2/Constants.php to see the NameIdFormat supported.
-				'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
-				// Usually x509cert and privateKey of the SP are provided by files placed at
-				// the certs folder. But we can also provide them with the following parameters
-				'x509cert' => '',
-				'privateKey' => '',
-				/*
-				 * Key rollover
-				 * If you plan to update the SP x509cert and privateKey
-				 * you can define here the new x509cert and it will be
-				 * published on the SP metadata so Identity Providers can
-				 * read them and get ready for rollover.
-				 */
-				// 'x509certNew' => '',
-
-			),
-
 			// Identity Provider Data that we want connected with our SP.
 			'idp' => array (
 				// Identifier of the IdP entity  (must be a URI)
