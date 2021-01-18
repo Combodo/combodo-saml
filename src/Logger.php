af80bf8a5557d3862a04e1cd40a532e41e50c345
bruno-ds <bruno.dasilva@combodo.com>
2021-01-18 14:56:26 +0100

    Rolback the legacy conf compatibility, the code is better without

--- src/Logger.php
+++ src/Logger.php
@@ -24,8 +24,6 @@ class Logger extends \LogAPI
             $sTargetFile = APPROOT.'log/saml.log';
         }
 
-        self::legacyConfHandler();
-
         parent::Enable($sTargetFile);
     }
 
@@ -36,54 +34,5 @@ class Logger extends \LogAPI
         }
 
         parent::Log($sLevel, $sMessage, $sChannel, $aContext);
-    }
-
-    private static function legacyConfHandler()
-    {
-        if (static::$m_oFileLog) {
-            return;
-        }
-
-        $oConfig = (static::$m_oMockMetaModelConfig !== null) ? static::$m_oMockMetaModelConfig :  \MetaModel::GetConfig();
-        if (!$oConfig instanceof Config)
-        {
-            return;
-        }
-
-        $deprecatedSettingValue = static::$bDebug = MetaModel::GetModuleSetting('combodo-saml', 'debug', null);
-        if ($deprecatedSettingValue !== false) {
-            //we only handle the legacy behavior of when the loggin was disabled it did still log errors.
-            return;
-        }
-
-        $sLogLevelMin = $oConfig->Get('log_level_min');
-
-//        if (isset($sLogLevelMin[static::CHANNEL_DEFAULT]) && $deprecatedSettingValue == null)
-//        {
-//            //This is the nominal case
-//            return;
-//        }
-
-        if (
-            (false == $deprecatedSettingValue) &&
-            (
-                (!isset($sLogLevelMin[static::CHANNEL_DEFAULT]))
-                ||
-                (static::LEVEL_ERROR != $sLogLevelMin[static::CHANNEL_DEFAULT])
-            )
-        )
-        {
-            //the legacy code was filtering out the log below Error  of the legacy setting was false
-            \IssueLog::Warning('The config "debug" for module "combodo-saml" is deprecated, you should use the "log_level_min" for the channel "'.self::CHANNEL_DEFAULT.'" instead.');
-            $sLogLevelMin[static::CHANNEL_DEFAULT] = static::LEVEL_ERROR;
-            $oConfig->Set('log_level_min', $sLogLevelMin);
-            return;
-        }
-
-//        if (! isset($sLogLevelMin[static::CHANNEL_DEFAULT]) && $deprecatedSettingValue != false)
-//        {
-//            //Per default, the legacy code was allowing the debug level, this is not the standard behavior, but, well, we choose to be futur proof and do not hjandle this speicficity
-//            return;
-//        }
     }
 }
