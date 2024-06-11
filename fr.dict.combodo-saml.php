524a391bc9b1bb2e15f31c315831fc0a2da9e3e6
Timothee <timothee.callet@combodo.com>
2024-06-11 16:58:59 +0200

    Automated dictionaries synchronization with english reference

--- fr.dict.combodo-saml.php
+++ fr.dict.combodo-saml.php
@@ -1,21 +1,23 @@
 <?php
 /**
- * @copyright   Copyright (C) 2010-2019 Combodo SARL
- * @license     https://www.combodo.com/documentation/combodo-software-license.html
+ * Localized data
  *
+ * @copyright Copyright (C) 2010-2024 Combodo SAS
+ * @license    https://opensource.org/licenses/AGPL-3.0
+ * 
  */
 /**
- * Localized data
+ *
  */
-
-Dict::Add('FR FR', 'French', 'Français', array(
-	'SAML:Error:UserNotAllowed' => 'Utilisateur non autorisé',
+Dict::Add('FR FR', 'French', 'Français', [
+	'Menu:SAMLConfiguration' => 'Configuration SAML',
+	'SAML:Error:CheckTheLogFileForMoreInformation' => 'Check the log file "log/saml.log" for more information.~~',
 	'SAML:Error:ErrorOccurred' => 'Une erreur est survenue',
+	'SAML:Error:Invalid_Attribute' => 'L\'authentification SAML a échoué car l\'attribut attendu \'%1$s\' n\'est pas présent dans le réponse de l\'Identity Provider (IdP). Consultez le fichier error.log pour plus d\'informations.',
 	'SAML:Error:NotAuthenticated' => 'Non authentifié',
+	'SAML:Error:UserNotAllowed' => 'Utilisateur non autorisé',
+	'SAML:Login:SignIn' => 'S\'identifier avec SAML',
+	'SAML:Login:SignInTooltip' => 'Cliquer ici pour s\'identifier avec le serveur SAML',
 	'SAML:SimpleSaml:GenerateSimpleSamlConf' => 'Générer la configuration SimpleSaml',
 	'SAML:SimpleSaml:Instructions' => 'Ajouter ces lignes à la configuration: simplesamlphp/metadata/saml20-sp-remote.php',
-    'SAML:Login:SignIn' => 'S\'identifier avec SAML',
-    'SAML:Login:SignInTooltip' => 'Cliquer ici pour s\'identifier avec le serveur SAML',
-	'Menu:SAMLConfiguration' => 'Configuration SAML',
-	'SAML:Error:Invalid_Attribute' => 'L\'authentification SAML a échoué car l\'attribut attendu \'%1$s\' n\'est pas présent dans le réponse de l\'Identity Provider (IdP). Consultez le fichier error.log pour plus d\'informations.',
-));
+]);
